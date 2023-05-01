809e0821e85f065a5c8ab21b9c4c5f69c2f03890
akabir360 <360sayeed@gmail.com>
2023-05-01 12:02:31 +1000

    Initial Commit

---
+++ MovieSearchView/MovieSearchViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearchViewApp.swift
+//  MovieSearchView
+//
+//  Created by Abu Sayeed Kabir on 1/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieSearchViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
